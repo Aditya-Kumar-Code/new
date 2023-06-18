893debae46b4f0b3f36e516d389870fae00d3936
Aditya Kumar <87271653+Aditya-Kumar-Code@users.noreply.github.com>
2023-06-19 01:27:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import WordCountCalculator from './component/WordCountCalculator';
+
+function App() {
+  return (
+    <WordCountCalculator />
+  );
+}
+
+export default App;
